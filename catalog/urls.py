6975724521deb9aac43c4ca4c03744741d9a7b1b
pphelps28 <p.w.phelps28@gmail.com>
2021-03-16 13:30:19 -0400

    First version of application moved into github

---
+++ catalog/urls.py
@@ -0,0 +1,39 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    # Class-based views use the .as_view() method
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors', views.AuthorListView.as_view(), name="authors"),
+    path('authors/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('borrowed/', views.LoanedBooksListView.as_view(), name='all-borrowed')
+]
+
+# Form Validation URL config.
+
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian,
+         name='renew-book-librarian'),
+]
+
+# For Author CRUD Functionality
+
+urlpatterns += [
+    path('author/create/', views.AuthorCreate.as_view(), name="author-create"),
+    path('author/<int:pk>/update/',
+         views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/',
+         views.AuthorDelete.as_view(), name='author-delete')
+]
+
+# For Book CRUD  Functionality
+urlpatterns += [
+    path('book/create/', views.BookCreate.as_view(), name="book-create"),
+    path('book/<int:pk>/update/',
+         views.BookUpdate.as_view(), name='book-update'),
+    path('book/<int:pk>/delete/',
+         views.BookDelete.as_view(), name='book-delete')
+]
