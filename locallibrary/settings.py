8d11f6f6f42f256613915a94f5111ed789ed19ca
pphelps28 <p.w.phelps28@gmail.com>
2021-03-16 14:05:02 -0400

    fixed configuration

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -130,9 +130,9 @@ LOGIN_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Heroku: Update database configuration from $DATABASE_URL.
+# Heroku: Update database configuration from $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
